c2196691512dc17831c88e8fab1d4ddadd05d061
christy313 <christycreativity@gmail.com>
2022-07-15 12:15:10 -0400

    save the version before refactoring add todo function

--- Script.js
+++ Script.js
@@ -16,58 +16,51 @@ const loadTodos = () => {
 
 window.onload = loadTodos;
 
-const addTodo = () => {
-  document.querySelector(".todo-input").addEventListener("keypress", (e) => {
-    const storedTodos = JSON.parse(localStorage.getItem("todos")) || [];
-    const content = document.querySelector(".todo-input").value;
-    if (!content) return;
-    if (e.key === "Enter") {
-      localStorage.setItem(
-        "todos",
-        JSON.stringify([
-          ...storedTodos,
-          { id: `${new Date().getTime()}`, content, completed: false },
-        ])
-      );
-      const addTodo = document.createElement("div");
-      addTodo.classList.add("todo");
-      addTodo.innerHTML = `
+// add todo
+document.querySelector(".todo-input").addEventListener("keypress", (e) => {
+  const storedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+  const content = document.querySelector(".todo-input").value;
+  if (!content) return;
+  if (e.key === "Enter") {
+    localStorage.setItem(
+      "todos",
+      JSON.stringify([
+        ...storedTodos,
+        { id: `${new Date().getTime()}`, content, completed: false },
+      ])
+    );
+    const addTodo = document.createElement("div");
+    addTodo.classList.add("todo");
+    addTodo.innerHTML = `
         <input class="todo-check" type="checkbox">
         <div class="todo-content">${escapeHtml(content)}</div>
         <button class="delete">x</button>
       `;
 
-      document.querySelector(".todos").appendChild(addTodo);
-      document.querySelector(".todo-input").value = "";
-    }
-  });
-};
-addTodo();
+    document.querySelector(".todos").appendChild(addTodo);
+    document.querySelector(".todo-input").value = "";
+  }
+});
 
-const completeTodo = () => {
-  document.querySelector(".todos").addEventListener("click", (e) => {
-    if (e.target.classList.contains("todo-check")) {
-      const checked = e.target.parentNode.classList;
-      e.target.checked ? checked.add("done") : checked.remove("done");
-    }
-  });
-};
-completeTodo();
+// complete todo
+document.querySelector(".todos").addEventListener("click", (e) => {
+  if (e.target.classList.contains("todo-check")) {
+    const checked = e.target.parentNode.classList;
+    e.target.checked ? checked.add("done") : checked.remove("done");
+  }
+});
 
 // delete todo
-const deleteTodo = () => {
-  let storedTodos = JSON.parse(localStorage.getItem("todos") || "[]");
-  document.querySelector(".todos").addEventListener("click", (e) => {
-    if (e.target.classList.contains("delete")) {
-      e.target.closest("div").remove();
-    }
-    storedTodos.filter((todo) => {
-      todo.content !== e.target.previousElementSibling.innerHTML;
-    });
-    localStorage.setItem("todos", JSON.stringify(storedTodos));
+let storedTodos = JSON.parse(localStorage.getItem("todos") || "[]");
+document.querySelector(".todos").addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.closest("div").remove();
+  }
+  storedTodos.filter((todo) => {
+    todo.content !== e.target.previousElementSibling.innerHTML;
   });
-};
-deleteTodo();
+  localStorage.setItem("todos", JSON.stringify(storedTodos));
+});
 
 const escapeHtml = (todoInput) => {
   return todoInput
